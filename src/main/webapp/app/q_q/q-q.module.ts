ff9190ad3c240a50459bf17e9ad22e48df7edbd9
Vaidotas Staugaitis <vs@sanityassembly.com>
2018-08-14 09:09:17 -0500

    generate employee module and employeeList component

--- src/main/webapp/app/q_q/q-q.module.ts
+++ src/main/webapp/app/q_q/q-q.module.ts
@@ -7,12 +7,14 @@ import { NavTopComponent } from './nav-top/nav-top.component';
 import { NavSideComponent } from './nav-side/nav-side.component';
 import { AlternativeMainComponent } from './alternative-main/alternative-main.component';
 import { RecordsModule } from './records/records.module';
+import { EmployeeModule } from './records/employee/employee.module';
 
 @NgModule({
   imports: [
     CommonModule,
     RecordsModule,
-    QQRoutingModule
+    QQRoutingModule,
+    EmployeeModule
   ],
   declarations: [NavTopComponent, NavSideComponent, AlternativeMainComponent]
 })
---
+++ src/main/webapp/app/q_q/records/employee/employee-list/employee-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jhi-employee-list',
+  templateUrl: './employee-list.component.html',
+  styles: []
+})
+export class EmployeeListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
