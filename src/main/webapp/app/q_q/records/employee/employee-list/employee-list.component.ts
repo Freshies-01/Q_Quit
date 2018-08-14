efd4c601b4989f577f57e2194d611cccd391c428
denverb <denverb@quinnox.com>
2018-08-14 09:57:49 -0500

    employee list

--- src/main/webapp/app/q_q/records/employee/employee-list/employee-list.component.ts
+++ src/main/webapp/app/q_q/records/employee/employee-list/employee-list.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { IEmployee } from "app/shared/model/employee.model";
+import { EmployeeService } from "app/entities/employee/employee.service";
+import { HttpErrorResponse, HttpResponse } from "@angular/common/http";
 
 @Component({
-  selector: 'jhi-employee-list',
-  templateUrl: './employee-list.component.html',
+  selector: "jhi-employee-list",
+  templateUrl: "./employee-list.component.html",
   styles: []
 })
 export class EmployeeListComponent implements OnInit {
+  employees: IEmployee[];
 
-  constructor() { }
+  constructor(private employeeService: EmployeeService) {}
 
-  ngOnInit() {
+  loadAll() {
+    this.employeeService.query().subscribe(
+      (res: HttpResponse<IEmployee[]>) => {
+        this.employees = res.body;
+      },
+      (res: HttpErrorResponse) => console.console.error(res.message)
+    );
   }
-
+  ngOnInit() {
+    this.loadAll();
+  }
 }
