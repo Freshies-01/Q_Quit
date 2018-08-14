9ce5eaa9ce2cbf9c2d3889c030b818d87df93017
lorence <lorencevquinnox@gmail.com>
2018-08-14 14:38:16 -0500

    began creating display for sa
    - created dummy employee data via json
    - installed angular material
    - started creating the table for sa

--- src/main/webapp/app/q_q/records/separation-application/separation-application.module.ts
+++ src/main/webapp/app/q_q/records/separation-application/separation-application.module.ts
@@ -1,19 +1,18 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { Routes } from '@angular/router';
-import { SeparationApplicationListComponent } from './separation-application-list.component';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { Routes } from "@angular/router";
+import { SeparationApplicationListComponent } from "./separation-application-list.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 export const separationApplicationRoute: Routes = [
   {
-    path: 'separationApplication',
+    path: "separationApplication",
     component: SeparationApplicationListComponent
   }
 ];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
+  imports: [CommonModule, BrowserAnimationsModule],
   declarations: [SeparationApplicationListComponent]
 })
-export class SeparationApplicationModule { }
+export class SeparationApplicationModule {}
