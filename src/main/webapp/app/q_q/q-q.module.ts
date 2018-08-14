692f2b43d987895fe7f9036f70f70f32b88f8330
Vaidotas Staugaitis <vs@sanityassembly.com>
2018-08-14 09:30:16 -0500

    generated a reports module

--- src/main/webapp/app/q_q/q-q.module.ts
+++ src/main/webapp/app/q_q/q-q.module.ts
@@ -7,12 +7,14 @@ import { NavTopComponent } from './nav-top/nav-top.component';
 import { NavSideComponent } from './nav-side/nav-side.component';
 import { AlternativeMainComponent } from './alternative-main/alternative-main.component';
 import { RecordsModule } from './records/records.module';
+import { ReportsModule } from './reports/reports.module';
 
 @NgModule({
   imports: [
     CommonModule,
     RecordsModule,
     QQRoutingModule,
+    ReportsModule,
   ],
   declarations: [NavTopComponent, NavSideComponent, AlternativeMainComponent]
 })
