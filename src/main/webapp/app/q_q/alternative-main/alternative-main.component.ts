2ee2791d46d2f0105e39176e28c4bc65f27d808e
Vaidotas Staugaitis <vs@sanityassembly.com>
2018-08-15 11:34:17 -0500

    when navigating without authentication, user login pop us i brough up.

--- src/main/webapp/app/q_q/alternative-main/alternative-main.component.ts
+++ src/main/webapp/app/q_q/alternative-main/alternative-main.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { LoginModalService, LoginService, Principal } from "app/core";
+
+import { Subscription } from "rxjs/index";
 
 @Component({
-    selector: 'jhi-alternative-main',
-    templateUrl: './alternative-main.component.html',
-    styleUrls: ['./alternative-main.component.css']
+  selector: "jhi-alternative-main",
+  templateUrl: "./alternative-main.component.html",
+  styleUrls: ["./alternative-main.component.css"]
 })
-export class AlternativeMainComponent implements OnInit {
+export class AlternativeMainComponent implements OnInit, OnDestroy {
+  private authStateSubscription: Subscription;
 
-    constructor() { }
+  constructor(
+    private loginModalService: LoginModalService,
+    private loginService: LoginService,
+    private principal: Principal
+  ) {}
 
-    ngOnInit() {
+  ngOnInit() {
+    if (!this.principal.isAuthenticated()) {
+      this.loginModalService.open();
     }
 
+    this.authStateSubscription = this.principal
+      .getAuthenticationState()
+      .subscribe(
+        value => {
+          if (value === null) {
+            this.loginModalService.open();
+          }
+        },
+        error => console.log(error)
+      );
+  }
+
+  ngOnDestroy() {
+    this.authStateSubscription.unsubscribe();
+  }
 }
