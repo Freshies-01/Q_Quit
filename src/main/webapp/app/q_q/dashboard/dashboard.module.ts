747ff0004d277824b06381d7c6df1fe354450762
Vaidotas Staugaitis <vs@sanityassembly.com>
2018-08-16 14:58:13 -0500

    initialize material dashboard

--- src/main/webapp/app/q_q/dashboard/dashboard.module.ts
+++ src/main/webapp/app/q_q/dashboard/dashboard.module.ts
@@ -1,20 +1,32 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { Routes } from '@angular/router';
-import { DashboardComponent } from './dashboard.component';
+import { Routes } from "@angular/router";
+import { DashboardComponent } from "./dashboard.component";
+import {
+  MatGridListModule,
+  MatCardModule,
+  MatMenuModule,
+  MatIconModule,
+  MatButtonModule
+} from "@angular/material";
 
 export const dashboardRoutes: Routes = [
   {
-    path: 'dashboard',
+    path: "dashboard",
     component: DashboardComponent
   }
-]
+];
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule
   ],
   declarations: [DashboardComponent]
 })
-export class DashboardModule { }
+export class DashboardModule {}
