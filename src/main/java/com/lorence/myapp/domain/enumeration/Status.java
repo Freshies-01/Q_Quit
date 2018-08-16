b5a32c2a295ce22fe1d6248aabb9a24a0da6486a
lorence <lorencevquinnox@gmail.com>
2018-08-16 16:22:10 -0500

    status changes
    - changed status enum values
    - linked status value to progress spinner
    - made status changes to wherever relevant

--- src/main/java/com/lorence/myapp/domain/enumeration/Status.java
+++ src/main/java/com/lorence/myapp/domain/enumeration/Status.java
@@ -4,11 +4,12 @@ package com.lorence.myapp.domain.enumeration;
  * The Status enumeration.
  */
 public enum Status {
-     
-    PENDING(1), 
-    IN_PROGRESS(2),
-    COMPLETED(3),
-    CLOSED(4);
+    
+    PENDING(0),
+    UNDER_REVIEW_FR(25),
+    EMPLOYEE_TASKS_IN_PROGRESS(50),
+    EMPLOYEE_TASKS_COMPLETED(75),
+    CLOSED_BY_HR(100);
 
     private final int status;
     Status(int status) { this.status = status; }
--- src/main/webapp/app/q_q/records/separation-application/separation-application-list.component.ts
+++ src/main/webapp/app/q_q/records/separation-application/separation-application-list.component.ts
@@ -1,17 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { SeparationApplicationService } from 'app/entities/separation-application/separation-application.service';
-import { ISeparationApplication } from 'app/shared/model/separation-application.model';
-import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { Component, OnInit } from "@angular/core";
+import { SeparationApplicationService } from "app/entities/separation-application/separation-application.service";
+import { ISeparationApplication } from "app/shared/model/separation-application.model";
+import { HttpErrorResponse, HttpResponse } from "@angular/common/http";
 
 @Component({
-  selector: 'jhi-separation-application-list',
-  templateUrl: './separation-application-list.component.html',
-  styles: []
+  selector: "jhi-separation-application-list",
+  templateUrl: "./separation-application-list.component.html",
+  styleUrls: ["./separation-application-list.component.css"]
 })
 export class SeparationApplicationListComponent implements OnInit {
   separationApplications: ISeparationApplication[];
 
-  constructor(private separationApplicationService: SeparationApplicationService) { }
+  mode = "determinate";
+  value = 66;
+
+  constructor(
+    private separationApplicationService: SeparationApplicationService
+  ) {}
 
   loadAll() {
     this.separationApplicationService.query().subscribe(
@@ -25,5 +30,4 @@ export class SeparationApplicationListComponent implements OnInit {
   ngOnInit() {
     this.loadAll();
   }
-
 }
