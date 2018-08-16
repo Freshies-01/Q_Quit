126be14559cdfb14a666178083b0b16d783ca1e1
Vaidotas Staugaitis <vs@sanityassembly.com>
2018-08-16 13:55:11 -0500

    Merge branch 'branch_Lorence_ActualV3' into AngularMaterialSetUp

--- src/main/java/com/lorence/myapp/domain/enumeration/Status.java
+++ src/main/java/com/lorence/myapp/domain/enumeration/Status.java
@@ -4,5 +4,13 @@ package com.lorence.myapp.domain.enumeration;
  * The Status enumeration.
  */
 public enum Status {
-    COMPLETED, PENDING, CLOSED, IN_PROGRESS
+     
+    PENDING(1), 
+    IN_PROGRESS(2),
+    COMPLETED(3),
+    CLOSED(4);
+
+    private final int status;
+    Status(int status) { this.status = status; }
+    public int getValue() { return this.status; }
 }
