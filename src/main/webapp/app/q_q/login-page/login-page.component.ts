7fec8c82f1fb4486a3a444db81cc62a6e7837e93
Vaidotas Staugaitis <vs@sanityassembly.com>
2018-08-16 15:25:24 -0500

    generated login page component

---
+++ src/main/webapp/app/q_q/login-page/login-page.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "jhi-login-page",
+  templateUrl: "./login-page.component.html",
+  styles: []
+})
+export class LoginPageComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
