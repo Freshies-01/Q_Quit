c3b2c4a7c1ef12d986d3abefd20e0f62d3af5c6d
Vaidotas Staugaitis <vs@sanityassembly.com>
2018-08-20 10:05:24 -0500

    imported angular material library to employee.module

--- src/main/webapp/app/q_q/records/employee/employee.module.ts
+++ src/main/webapp/app/q_q/records/employee/employee.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
 import { EmployeeListComponent } from "app/Q_Q/records/employee/employee-list/employee-list.component";
 import { EmployeeChangeComponent } from "./employee-change/employee-change.component";
+import { AngularMaterialModule } from "app/shared/angular-material.module";
 
 @NgModule({
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, AngularMaterialModule],
   declarations: [EmployeeListComponent, EmployeeChangeComponent]
 })
 export class MyEmployeeModule {}
