e46fffc9b4351fdbd927d5dc1c7d25bc31e0d3e4
Vaidotas Staugaitis <vs@sanityassembly.com>
2018-08-20 08:24:25 -0500

    Employee change form

--- src/main/webapp/app/q_q/records/employee/employee.module.ts
+++ src/main/webapp/app/q_q/records/employee/employee.module.ts
@@ -1,19 +1,18 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { Routes } from '@angular/router';
-import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { Routes } from "@angular/router";
+import { EmployeeListComponent } from "app/Q_Q/records/employee/employee-list/employee-list.component";
+import { EmployeeChangeComponent } from "app/q_q/records/employee/employee-change/employee-change.component";
 
 export const employeeRoutes: Routes = [
   {
-    path: 'employee',
+    path: "employee",
     component: EmployeeListComponent
   }
-]
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: [EmployeeListComponent]
+  imports: [CommonModule],
+  declarations: [EmployeeListComponent, EmployeeChangeComponent]
 })
-export class EmployeeModule { }
+export class EmployeeModule {}
