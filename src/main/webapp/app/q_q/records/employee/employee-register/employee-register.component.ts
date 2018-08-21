12e58fcde33068d4928eefbfe6bcf69befc4d27d
Vaidotas Staugaitis <vs@sanityassembly.com>
2018-08-21 14:55:44 -0500

    generated employee register component

---
+++ src/main/webapp/app/q_q/records/employee/employee-register/employee-register.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "jhi-employee-register",
+  templateUrl: "./employee-register.component.html",
+  styles: []
+})
+export class EmployeeRegisterComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
--- src/main/webapp/app/q_q/records/employee/employee.module.ts
+++ src/main/webapp/app/q_q/records/employee/employee.module.ts
@@ -6,6 +6,7 @@ import { ReactiveFormsModule } from "@angular/forms";
 import { EmployeeListComponent } from "app/Q_Q/records/employee/employee-list/employee-list.component";
 import { EmployeeChangeComponent } from "./employee-change/employee-change.component";
 import { AngularMaterialModule } from "app/shared/angular-material.module";
+import { EmployeeRegisterComponent } from "./employee-register/employee-register.component";
 
 @NgModule({
   imports: [
@@ -14,6 +15,10 @@ import { AngularMaterialModule } from "app/shared/angular-material.module";
     AngularMaterialModule,
     ReactiveFormsModule
   ],
-  declarations: [EmployeeListComponent, EmployeeChangeComponent]
+  declarations: [
+    EmployeeListComponent,
+    EmployeeChangeComponent,
+    EmployeeRegisterComponent
+  ]
 })
 export class MyEmployeeModule {}
