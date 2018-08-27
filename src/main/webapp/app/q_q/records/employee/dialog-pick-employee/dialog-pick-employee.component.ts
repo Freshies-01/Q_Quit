f3d5111f80b091fcbd6e47a24d11b5a906e8fcc9
Vaidotas Staugaitis <vs@sanityassembly.com>
2018-08-26 21:33:22 -0500

    DialogPickEmployeeComponent now returns an employee object

--- src/main/webapp/app/q_q/records/employee/dialog-pick-employee/dialog-pick-employee.component.ts
+++ src/main/webapp/app/q_q/records/employee/dialog-pick-employee/dialog-pick-employee.component.ts
@@ -1,17 +1,16 @@
-import { Component, OnInit, Inject, AfterContentInit } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  Inject,
+  AfterContentInit,
+  OnDestroy
+} from "@angular/core";
 import { MAT_DIALOG_DATA, MatDialogRef, MatDialog } from "@angular/material";
 import { Employee } from "app/shared/model/employee.model";
 import { EmployeeService } from "app/entities/employee/employee.service";
 
-import { fromEvent, of } from "rxjs";
-import { ajax } from "rxjs/ajax";
-import {
-  map,
-  filter,
-  debounceTime,
-  distinctUntilChanged,
-  switchMap
-} from "rxjs/operators";
+import { fromEvent, Subscription } from "rxjs";
+import { map, debounceTime, distinctUntilChanged } from "rxjs/operators";
 
 export interface DialockPickEmployeeData {
   employee: Employee;
@@ -22,19 +21,39 @@ export interface DialockPickEmployeeData {
   templateUrl: "./dialog-pick-employee.component.html",
   styles: []
 })
-export class DialogPickEmployeeComponent implements OnInit, AfterContentInit {
+export class DialogPickEmployeeComponent
+  implements OnInit, AfterContentInit, OnDestroy {
   allEmployees: Employee[];
   filteredEmployees: Employee[];
-  searchBox: HTMLInputElement;
-
-  typeahead$;
+  typeAheadSubscription: Subscription;
 
   constructor(
     private dialogRef: MatDialogRef<DialogPickEmployeeComponent>,
     private employeeService: EmployeeService
   ) {}
 
-  SearchInFirstAndLastName(searchTerm: string) {
+  ngOnInit() {
+    this.employeeService.query().subscribe(result => {
+      this.allEmployees = result.body;
+      this.filteredEmployees = this.allEmployees;
+    });
+  }
+
+  ngAfterContentInit() {
+    const searchBox = document.getElementById("search-box") as HTMLInputElement;
+
+    const searchBarObservable = fromEvent(searchBox, "input").pipe(
+      map((event: KeyboardEvent) => (event.target as HTMLOutputElement).value),
+      debounceTime(10),
+      distinctUntilChanged()
+    );
+
+    this.typeAheadSubscription = searchBarObservable.subscribe(data => {
+      this.FilterBySearchTerm(data);
+    });
+  }
+
+  FilterBySearchTerm(searchTerm: string) {
     this.filteredEmployees = this.allEmployees.filter(item => {
       const firstAndLastName = `${item.user.firstName} ${item.user.lastName}`;
       const RegularExpression = new RegExp(searchTerm, "gmi");
@@ -42,27 +61,11 @@ export class DialogPickEmployeeComponent implements OnInit, AfterContentInit {
     });
   }
 
-  onNoClick(): void {
-    this.dialogRef.close();
+  onEmployeeSelected(employee: Employee) {
+    this.dialogRef.close(employee);
   }
 
-  ngOnInit() {
-    this.employeeService.query().subscribe(result => {
-      this.allEmployees = result.body;
-    });
-  }
-  ngAfterContentInit() {
-    this.searchBox = document.getElementById("search-box") as HTMLInputElement;
-
-    this.typeahead$ = fromEvent(this.searchBox, "input").pipe(
-      map((event: KeyboardEvent) => (event.target as HTMLOutputElement).value),
-      debounceTime(10),
-      distinctUntilChanged()
-    );
-
-    this.typeahead$.subscribe(data => {
-      console.log(data);
-      this.SearchInFirstAndLastName(data);
-    });
+  ngOnDestroy() {
+    this.typeAheadSubscription.unsubscribe();
   }
 }
