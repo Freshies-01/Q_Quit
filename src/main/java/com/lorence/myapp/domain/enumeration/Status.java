e405fa357197f5b2ca4d4d61beaf3096e7e260db
denverb <denverb@quinnox.com>
2018-08-28 14:57:29 -0500

    fixed employee filters

--- src/main/java/com/lorence/myapp/domain/enumeration/Status.java
+++ src/main/java/com/lorence/myapp/domain/enumeration/Status.java
@@ -5,13 +5,15 @@ package com.lorence.myapp.domain.enumeration;
  */
 public enum Status {
     
-    PENDING(0),
-    UNDER_REVIEW_FR(25),
-    EMPLOYEE_TASKS_IN_PROGRESS(50),
-    EMPLOYEE_TASKS_COMPLETED(75),
-    CLOSED_BY_HR(100);
+    PENDING(0, "Pending"),
+    UNDER_REVIEW_FR(25, "Under Review"),
+    EMPLOYEE_TASKS_IN_PROGRESS(50, "Employee Tasks In Progress"),
+    EMPLOYEE_TASKS_COMPLETED(75, "Employee Tasks Complete"),
+    CLOSED_BY_HR(100, "Closed By HR");
 
     private final int status;
-    Status(int status) { this.status = status; }
+    private final String name;
+    Status(int status) { this.status = status; this.name = "";}
+    Status(int status, String name) { this.status = status; this.name = name; }
     public int getValue() { return this.status; }
 }
