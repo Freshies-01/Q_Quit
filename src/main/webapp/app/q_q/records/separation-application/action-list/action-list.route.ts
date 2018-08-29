88001188dd10fc7d716b2dae6c179492b4382bf7
Vaidotas Staugaitis <vs@sanityassembly.com>
2018-08-29 14:39:31 -0500

    Employee Value Accessor is now correctly sized when value is null

--- src/main/webapp/app/q_q/records/separation-application/action-list/action-list.route.ts
+++ src/main/webapp/app/q_q/records/separation-application/action-list/action-list.route.ts
@@ -5,8 +5,13 @@ import {
   RouterStateSnapshot
 } from "@angular/router";
 import { Injectable } from "@angular/core";
-import { ActionListComponent } from "./action-list.component";
-import { ActionDeleteDialogComponent } from "../../../../entities/action/action-delete-dialog.component";
+import { ActionDeletePopupComponent } from "../../../../entities/action/action-delete-dialog.component";
+import { IAction, Action } from "app/shared/model/action.model";
+import { ActionService } from "app/entities/action";
+import { map } from "rxjs/operators";
+import { HttpResponse } from "@angular/common/http";
+import { of } from "rxjs";
+import { UserRouteAccessService } from "app/core";
 
 @Injectable({ providedIn: "root" })
 export class ActionResolve implements Resolve<IAction> {
