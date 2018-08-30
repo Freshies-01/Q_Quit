4109ee2725567cbc1f2b41bd1584aee3c4dce262
Vai <vaidorass@live.com>
2018-08-30 11:28:07 -0500

    enum enumarator pipe changed to work with string enums

--- src/main/webapp/app/shared/util/EnumKeyPipe/enum-key.pipe.ts
+++ src/main/webapp/app/shared/util/EnumKeyPipe/enum-key.pipe.ts
@@ -4,10 +4,10 @@ import { Pipe, PipeTransform } from "@angular/core";
 export class KeysPipe implements PipeTransform {
   transform(value, args: string[]): any {
     const keys = [];
+    console.log(value);
+    // tslint:disable-next-line:forin
     for (const enumMember in value) {
-      if (!isNaN(parseInt(enumMember, 10))) {
-        keys.push({ key: enumMember, value: value[enumMember] });
-      }
+      keys.push({ key: enumMember, value: value[enumMember] });
     }
     return keys;
   }
